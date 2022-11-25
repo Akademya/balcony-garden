21d0d156e4cb93fd01918836f1c820409329528f
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2022-11-26 01:07:41 +0200

    start balcony

---
+++ src/scenes/balcony/areas/wc_area_2D.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton \
+	and event.button_index == BUTTON_LEFT \
+	and event.is_pressed():
+		print("Clicked WateringCan")
+
+
+func _on_WCArea2D_mouse_entered():
+	get_node("Sprite").material.set_shader_param("is_on", true)
+
+
+func _on_WCArea2D_mouse_exited():
+	get_node("Sprite").material.set_shader_param("is_on", false)
