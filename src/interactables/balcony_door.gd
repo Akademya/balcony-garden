3c9724584b37814a2cb2ceb0efd51246d270d985
Thomas <t.wild155@gmail.com>
2022-11-26 23:01:16 +0200

    add working balcony door

--- src/interactables/balcony_door.gd
+++ src/interactables/balcony_door.gd
@@ -5,7 +5,8 @@ func _ready() -> void:
 
 
 func use():
-	print("I'm a door lol")
+	if get_tree().get_nodes_in_group("SceneManager")[0].has_method("load_scene"):
+		get_tree().get_nodes_in_group("SceneManager")[0].load_scene("balcony_scene")
 
 
 func _on_Interactable_body_entered(body: Node) -> void:
