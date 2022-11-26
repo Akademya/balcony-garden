383b562df52eb4bab799f6dbd6292b9001e8519c
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2022-11-26 11:39:21 +0200

    changes to room.gd

--- src/scenes/room.gd
+++ src/scenes/room.gd
@@ -4,16 +4,16 @@ onready var camera = $Camera2D
 var speed = 100
 var direction = Vector2.ZERO
 
-func move_cam(_delta: float):
-	camera.global_position.x += int(direction.x * speed * _delta)
-	camera.global_position.y += int(direction.y * speed * _delta)
+func move_cam(delta: float):
+	camera.global_position.x += direction.x * speed * delta
+	camera.global_position.y += direction.y * speed * delta
 
 func get_input():
 	direction = Vector2.ZERO
 	direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
 	direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 
-func _process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	# very niceo, very clean code :)
 	get_input()
-	move_cam(_delta)
+	move_cam(delta)
