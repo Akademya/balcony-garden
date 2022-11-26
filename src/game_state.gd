d031d10492d44b5b12e0fbfd306c8345a2bc19be
Foica Serban <serban.foica@gmail.com>
2022-11-26 19:58:22 +0200

    add game_state singleton

---
+++ src/game_state.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var global_money = 0
---
+++ src/scenes/Shop/ShopHBox.gd
@@ -0,0 +1,9 @@
+extends HBoxContainer
+
+
+func _ready():
+	var money = get_tree().get_nodes_in_group("SceneManager")[0].global_money
+	
+
+func _process(delta):
+	pass
