7a2b36a6aafa29cd720e53217f618d8e3ce14aea
Thomas <t.wild155@gmail.com>
2022-11-26 22:22:30 +0200

    add player

--- src/interactables/balcony_door.gd
+++ src/interactables/balcony_door.gd
@@ -4,5 +4,9 @@ func _ready() -> void:
 	pass
 
 
-func _on_Area2D_area_entered(area: Area2D) -> void:
-	pass
+func use():
+	print("I'm a door lol")
+
+
+func _on_Interactable_body_entered(body: Node) -> void:
+	body.interactable = self
---
+++ src/player/player.gd
@@ -0,0 +1,42 @@
+extends KinematicBody2D
+
+var interactable : Node
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+var speed : int = 50
+var velocity : Vector2 
+var direction : Vector2
+var target : Vector2
+var last_direction: Vector2
+
+func find_path(mouse_pos): 
+	target = mouse_pos
+	direction = (mouse_pos - self.global_position).normalized()
+	velocity = direction * speed
+
+func interact():
+	if interactable and interactable.has_method("use"):
+		interactable.use()
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		find_path(get_viewport().get_mouse_position())
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		interact()
+
+func _physics_process(_delta: float) -> void:
+	# animate
+	if velocity != Vector2.ZERO:
+		animationTree.set("parameters/idle/blend_position", velocity)
+		animationTree.set("parameters/run/blend_position", velocity)
+		animationState.travel("run")
+	else: 
+		animationState.travel("idle")
+	
+	# move to click pos
+	if self.global_position.distance_to(target) > 1:
+		velocity = move_and_slide(velocity)
+	else:
+		velocity = Vector2.ZERO
