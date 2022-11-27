5ff65583df69edca6a77a7bdabdb11da43afe94a
Thomas <t.wild155@gmail.com>
2022-11-27 04:06:54 +0200

    add transition + bed interact

--- src/game_state.gd
+++ src/game_state.gd
@@ -1,3 +1,4 @@
 extends Node
 
 var global_money = 10
+var player_position : Vector2 = Vector2(69, 69)
--- src/interactables/bed.gd
+++ src/interactables/bed.gd
@@ -4,7 +4,11 @@ func _ready() -> void:
 	pass
 
 func use():
-	print("zzZ")
+	var transition = get_parent().get_node("CanvasLayer/Transition")
+	transition.fade_out()
+	
+	if get_tree().get_nodes_in_group("SceneManager")[0].has_method("load_scene"):
+		get_tree().get_nodes_in_group("SceneManager")[0].load_scene("room_scene")
 
 func _on_Interactable_body_entered(body: Node) -> void:
 	body.interactable = self
