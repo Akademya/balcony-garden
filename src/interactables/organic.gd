cf28ece293440e63ccd8e55ce4ef9df8a9a616a2
Thomas <t.wild155@gmail.com>
2022-11-27 13:31:25 +0200

    add trashcans

---
+++ src/interactables/organic.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+var is_mouse_inside: bool = false
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == BUTTON_RIGHT and event.pressed and is_mouse_inside:
+		print("pressed organic")
+
+func _on_Organic_mouse_entered() -> void:
+	is_mouse_inside = true
+
+
+func _on_Organic_mouse_exited() -> void:
+	is_mouse_inside = false
